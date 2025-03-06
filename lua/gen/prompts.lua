8d80c4acfb44eaccb9e3f8b02cee00faa295f188
tinhat <tinhat@tutanota.de>
2025-03-06 10:24:11 -0800

    Added: added and modified prompts for more concise responses
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
            modified:   lua/gen/prompts.lua

--- lua/gen/prompts.lua
+++ lua/gen/prompts.lua
@@ -1,8 +1,12 @@
 return {
-    Generate = { prompt = "$input", replace = true },
+    Generate = {
+        prompt = "$input\nDO NOT output any explainations or anything except for code only",
+        replace = true,
+    },
     Chat = { prompt = "$input" },
     Summarize = { prompt = "Summarize the following text:\n$text" },
-    Ask = { prompt = "Regarding the following text, $input:\n$text" },
+    Ask = { prompt = "$input" },
+    Ask_Text = { prompt = "Regarding the following text, $input:\n$text" },
     Change = {
         prompt = "Change the following text, $input, just output the final text without additional quotes around it:\n$text",
         replace = true,
